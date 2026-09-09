8e15f5d86b2390b89ac0d83636863e1eb7b4d099
marc2016 <marc@lammers.dev>
2026-09-09 09:09:35 +0200

    chore: configure playwright to always use list and html reporters

--- frontend/playwright.config.ts
+++ frontend/playwright.config.ts
@@ -6,7 +6,7 @@ export default defineConfig({
   forbidOnly: !!process.env.CI,
   retries: process.env.CI ? 2 : 0,
   workers: 1,
-  reporter: process.env.CI ? [['list'], ['html', { open: 'never' }]] : 'list',
+  reporter: [['list'], ['html', { open: 'never' }]],
   use: {
     baseURL: 'http://localhost:5188',
     trace: 'on-first-retry',
